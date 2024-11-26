b65260beecf190996880f9ede1ed4a7b6c35048f
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2024-11-26 15:12:57 +0800

    Update Base.cs
    dadada

--- Assets/Test/Base.cs
+++ Assets/Test/Base.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Base : MonoBehaviour
+public class Base : MonoBehaviour
 {
     
 
@@ -24,19 +24,5 @@ public abstract class Base : MonoBehaviour
     public int p3defences = 1114242111;
     public int p3speed = 2022020;
 
-    public void Stats()
-    {
-        Debug.Log($"Player 1 Health: {p1health} \r\n Player 1 Mana: {p1mana} \r\n Player 1 Attack: {p1attack} \r\n Player 1 Defence: {p1defences} \r\n Player 1 Speed: {p1speed}");
-
-    }
-    public void STats()
-    {
-        Debug.Log($"Player 2 Health: {p2health} \r\n Player 2 Mana: {p2mana} \r\n Player 2 Attack: {p2attack} \r\n Player 2 Defence: {p2defences} \r\n Player 2 Speed: {p2speed}");
-
-    }
-    public void Random()
-    {
-        Debug.Log($"Player 2 Health: {p2health} \r\n Player 2 Mana: {p2mana} \r\n Player 2 Attack: {p2attack} \r\n Player 2 Defence: {p2defences} \r\n Player 2 Speed: {p2speed}");
-
-    }
+   
 }
