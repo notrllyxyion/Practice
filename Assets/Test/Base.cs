faa2e1c71f8a5de9b548c413ff017d11c3ccc86f
notrllyxyion <mikelreuelramos69@gmail.com>
2024-11-26 22:27:01 +0800

    fds

--- Assets/Test/Base.cs
+++ Assets/Test/Base.cs
@@ -4,25 +4,25 @@ using UnityEngine;
 
 public class Base : MonoBehaviour
 {
-    
+    protected string overallstats;
 
-    public int p1health = 1522;
-    public int p1mana = 1521;
-    public int p1attack = 111241;
-    public int p1defences = 11124111;
-    public int p1speed = 2024522020;
+    protected int p1health = 1522;
+    protected int p1mana = 1521;
+    protected int p1attack = 111241;
+    protected int p1defences = 11124111;
+    protected int p1speed = 2024522020;
 
-    public int p2health = 124422;
-    public int p2mana = 1241;
-    public int p2attack = 11124241;
-    public int p2defences = 14211111;
-    public int p2speed = 20204220;
+    protected int p2health = 124422;
+    protected int p2mana = 1241;
+    protected int p2attack = 11124241;
+    protected int p2defences = 14211111;
+    protected int p2speed = 20204220;
 
-    public int p3health = 124242;
-    public int p3mana = 14242241;
-    public int p3attack = 11124241;
-    public int p3defences = 1114242111;
-    public int p3speed = 2022020;
+    protected int p3health = 124242;
+    protected int p3mana = 14242241;
+    protected int p3attack = 11124241;
+    protected int p3defences = 1114242111;
+    protected int p3speed = 2022020;
 
    
 }
--- Assets/Test/PlayerButtons.cs
+++ Assets/Test/PlayerButtons.cs
@@ -1,33 +1,32 @@
 
 using UnityEngine;
-using TMPro;
-using JetBrains.Annotations;
 
 public class PlayerButtons : Base
 {
 
-    public int Text =
     public void OnGUI()
     {
 
-       
-            
-       
-    
-        if (GUI.Button(new Rect(50, 50, 100, 100), "Button - player 1"))
+
+
+        if (GUI.Button(new Rect(20, 50, 100, 100), "Button - player 1"))
         {
-          
+            overallstats = $"Health: {p1health} \r\n Mana {p1mana} \r\n Attack: {p1attack} \r\n Defense: {p2defences} \r\n Speed: {p2speed}";
         }
 
-        if (GUI.Button(new Rect(50, 50, 250, 250), "Button - player 2"))
+        if (GUI.Button(new Rect(750, 100, 250, 250), "Button - player 2"))
         {
-          Text = 
+            overallstats = $"Attack: {p2health} \r\n Mana {p2mana} \r\n Attack: {p2attack} \r\n Defense: {p2defences} \r\n Speed: {p2speed}";
+        }
+
+        if (GUI.Button(new Rect(90, 750, 150, 150), "Button - player 33"))
+        {
+
         }
 
 
 
-
-        GUI.Label(new Rect(500, 200, 100, 100), Text.ToString);
+        GUI.Label(new Rect(500, 200, 500, 500), overallstats.ToString());
 
     }
 }
